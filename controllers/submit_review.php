07bd4a00e104806600e1969b4047ed16cd12aeb8
HafsaNishu <23-51223-1@student.aiub.edu>
2026-01-15 13:26:17 +0600

    controllers/submit_review

---
+++ controllers/submit_review.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+require_once '../config/db.php';
+require_once '../models/reviewModel.php';
+
+// Security Check
+if (!isset($_SESSION['user_id']) || $_SESSION['role'] != 'reviewer') {
+    header("Location: login.php");
+    exit;
+}
+
+$reviewer_id = $_SESSION['user_id'];
+$message = "";
+$error = "";
+
+?>
