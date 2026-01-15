0865554f100df73efea939efed3b42ccac857e5b
HafsaNishu <23-51223-1@student.aiub.edu>
2026-01-15 20:41:49 +0600

    controllers/create_admin

--- controllers/create_admin.php
+++ controllers/create_admin.php
@@ -3,4 +3,13 @@ session_start();
 require_once '../config/db.php';
 require_once '../models/adminModel.php';
 
+// 1. Security Check: Only Admins can enter this page
+if (!isset($_SESSION['user_id']) || $_SESSION['role'] != 'admin') {
+    header("Location: login.php");
+    exit;
+}
+
+$message = "";
+$error = "";
+
 ?>
