94f5e8c1d921719be3bb24a03b2166960beaafec
HafsaNishu <23-51223-1@student.aiub.edu>
2026-01-15 13:48:10 +0600

    controllers/submit_review

--- controllers/submit_review.php
+++ controllers/submit_review.php
@@ -13,4 +13,18 @@ $reviewer_id = $_SESSION['user_id'];
 $message = "";
 $error = "";
 
+// 1. Get Resume ID from URL
+if (isset($_GET['id'])) {
+    $resume_id = $_GET['id'];
+    $resume_data = getResumeDetails($conn, $resume_id);
+
+    // Basic Validation: Ensure this resume exists and is assigned to THIS reviewer
+    if (!$resume_data || $resume_data['assigned_to'] != $reviewer_id) {
+        die("Access Denied: You are not assigned to this resume.");
+    }
+} else {
+    header("Location: reviewer_dashboard.php");
+    exit;
+}
+
 ?>
