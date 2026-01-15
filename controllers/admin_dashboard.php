ebe45f9e04eb5a1f64d89cc41acdeadf39443adc
HafsaNishu <23-51223-1@student.aiub.edu>
2026-01-15 19:32:29 +0600

    controllers/admin_dashboard

--- controllers/admin_dashboard.php
+++ controllers/admin_dashboard.php
@@ -16,7 +16,21 @@ $error = "";
 // 2. Handle Form: Assign Reviewer
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['assign_btn'])) {
     $resume_id = $_POST['resume_id'];
-    
+
+     // --- VALIDATION FIX ---
+    // Check if a reviewer was actually selected
+    if (!isset($_POST['reviewer_id']) || empty($_POST['reviewer_id'])) {
+        $error = "Error: You must select a reviewer from the list.";
+    } 
+    else {
+        $reviewer_id = $_POST['reviewer_id'];
+        
+        if (assignReviewer($conn, $resume_id, $reviewer_id)) {
+            $message = "Reviewer assigned successfully!";
+        } else {
+            $error = "Failed to assign reviewer.";
+        }
+    }
     
 }
 
