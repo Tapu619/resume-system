882430a53c0936e55c14468782abf625a299485e
Tapu619 <zahedultapu123@gmail.com>
2026-01-15 23:57:16 +0600

    handling review model

---
+++ models/reviewModel.php
@@ -0,0 +1,16 @@
+<?php
+require_once '../config/db.php';
+
+// 1. Get all resumes assigned to THIS specific reviewer
+function getAssignedResumes($conn, $reviewer_id) {
+    // We join with the 'users' table to get the Job Seeker's name
+    $sql = "SELECT resumes.*, users.full_name AS seeker_name 
+            FROM resumes 
+            JOIN users ON resumes.user_id = users.id 
+            WHERE resumes.assigned_to = '$reviewer_id' 
+            ORDER BY resumes.upload_date DESC";
+            
+    $result = mysqli_query($conn, $sql);
+    return mysqli_fetch_all($result, MYSQLI_ASSOC);
+}
+?>
