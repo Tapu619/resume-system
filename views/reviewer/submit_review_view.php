46196607e2c04ce79ae458b3ff212ad490142046
vagabond-rafi <vagabondrafiu40@gmail.com>
2026-01-15 10:24:52 +0600

    views-reviewer-submit_review_view

---
+++ views/reviewer/submit_review_view.php
@@ -0,0 +1,16 @@
+!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Grade Resume</title>
+    <link rel="stylesheet" href="../assets/css/style.css">
+</head>
+<body>
+
+<div class="container" style="max-width: 800px;">
+    
+    <a href="reviewer_dashboard.php" class="btn-link">← Back to Dashboard</a>
+    
+    <div class="header">
+        <h2>Grade Resume: <?php echo $resume_data['seeker_name']; ?></h2>
+    </div>
